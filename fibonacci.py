906ded4e4075bcbc804009f829bdc9099d253897
andresmiqueas <andresmiqueas27@gmail.com>
2023-03-12 21:19:01 -0300

    fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -3,7 +3,6 @@
 * La serie Fibonacci se compone por una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores.
 * 0, 1, 1, 2, 3, 5, 8, 13...
 
-"""
 
 def fibonacci(n): # n es la posicion del numero en la secuencia
     a = 0
@@ -17,3 +16,20 @@ def fibonacci(n): # n es la posicion del numero en la secuencia
 
 for x in range(50):
     print(fibonacci(x))
+
+"""
+
+def fibonacci(): 
+
+    n1 = 0 #PARA EMPEZAR TENGO QUE SUMAR 1 Y 0
+    n2 = 1
+
+    for index in range(1, 51): #HAGO UN BUCLE DE INDEX ENTRE 1 Y 51 (SACO LOS PRIMEROS 50)
+ 
+        print(n1) #PRINTEO LOS NUMEROS EMPEZANDO DESDE EL 0
+        n3 = n1 + n2 #CREO N3 QUE ES EL RESULTADO DE LA SUMA DE 0 + 1 
+        n1 = n2 #ASIGO EL NUEVO VALOR A N1
+        n2 = n3 #ASIGO EL NUEVO VALOR A N2
+
+fibonacci()
+
